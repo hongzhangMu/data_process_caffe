c97e7601dfbb89f6a741b70a81ea88701e649330
mhongzhang <mhongzhang@infervision.com>
2017-06-27 09:28:51 +0800

    fix some bugs

--- FFT.py
+++ FFT.py
@@ -10,26 +10,33 @@ def graph_spectrogram(wav_file,f):
     plt.axis('off')
     filename=wav_file
     file=filename[48:]
-    path2='/media/tx-eva-08/data/workspace/dr3/'+f+'/'+file
-    path3='/media/tx-eva-08/data/workspace/dr3/'+f+'/'
+    path2='/media/tx-eva-08/data/workspace/dr7/'+f+'/'+file
+    path3='/media/tx-eva-08/data/workspace/dr7/'+f+'/'
     if os.path.exists(path3):
-        return True
-    os.mkdir(path3)
+        plt.savefig(path2+'.png',
+            dpi=100, # Dots per inch
+            frameon='false',
+            aspect='normal',
+            bbox_inches='tight',
+            pad_inches=0) # Spectrogram saved as a .png
+    else:
+        os.mkdir(path3)
+        plt.savefig(path2+'.png',
+            dpi=100, # Dots per inch
+            frameon='false',
+            aspect='normal',
+            bbox_inches='tight',
+            pad_inches=0) # Spectrogram saved as a .png
     print file
     print "path2:::"+path2
-    plt.savefig(path2+'.png',
-                dpi=100, # Dots per inch
-                frameon='false',
-                aspect='normal',
-                bbox_inches='tight',
-                pad_inches=0) # Spectrogram saved as a .png 
+    
     print "saved ........."
 def get_wav_info(wav_file):
     rate, data = wavfile.read(wav_file)
     return rate, data
 
 if __name__ == '__main__': # Main function
-    rootdir = r'/media/tx-eva-08/data/workspace/TRAIN/DR3/'
+    rootdir = r'/media/tx-eva-08/data/workspace/TRAIN/DR7/'
     for parent, dirnames, filenames in os.walk(rootdir):
         # for filename in filenames:  
         print "parent is: " + parent
